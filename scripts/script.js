bd8f9183b7b8c984a274416eea0719001b81221c
Vineeth S <svineth.face@gmail.com>
2018-10-23 00:21:36 +0530

    v1

---
+++ scripts/script.js
@@ -0,0 +1,107 @@
+/**
+ * @name handleFail
+ * @param err - error thrown by any function
+ * @description Helper function to handle errors
+ */
+let handleFail = function(err){
+    console.log("Error : ", err);
+};
+
+// Queries the container in which the remote feeds belong
+let remoteContainer= document.getElementById("remote-container");
+let canvasContainer =document.getElementById("canvas-container");
+/**
+ * @name addVideoStream
+ * @param streamId
+ * @description Helper function to add the video stream to "remote-container"
+ */
+function addVideoStream(streamId){
+    let streamDiv=document.createElement("div"); // Create a new div for every stream
+    streamDiv.id=streamId;                       // Assigning id to div
+    streamDiv.style.transform="rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
+    remoteContainer.appendChild(streamDiv);      // Add new div to container
+}
+/**
+ * @name removeVideoStream
+ * @param evt - Remove event
+ * @description Helper function to remove the video stream from "remote-container"
+ */
+function removeVideoStream (evt) {
+    let stream = evt.stream;
+    stream.stop();
+    let remDiv=document.getElementById(stream.getId());
+    remDiv.parentNode.removeChild(remDiv);
+    console.log("Remote stream is removed " + stream.getId());
+}
+
+function addCanvas(streamId){
+    let canvas=document.createElement("canvas");
+    canvas.id='canvas'+streamId;
+    canvasContainer.appendChild(canvas);
+    let ctx = canvas.getContext('2d');
+    let video=document.getElementById(`video${streamId}`);
+
+    video.addEventListener('loadedmetadata', function() {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+    });
+
+    video.addEventListener('play', function() {
+        var $this = this; //cache
+        (function loop() {
+            if (!$this.paused && !$this.ended) {
+                ctx.drawImage($this, 0, 0);
+                setTimeout(loop, 1000 / 30); // drawing at 30fps
+            }
+        })();
+    }, 0);
+}
+
+// Client Setup
+// Defines a client for RTC
+let client = AgoraRTC.createClient({
+    mode: 'live',
+    codec: "h264"
+});
+
+// Client Setup
+// Defines a client for Real Time Communication
+client.init("<---app id--->",() => console.log("AgoraRTC client initialized") ,handleFail);
+
+// The client joins the channel
+client.join(null,"any-channel",null, (uid)=>{
+
+    // Stream object associated with your web cam is initialized
+    let localStream = AgoraRTC.createStream({
+        streamID: uid,
+        audio: false,
+        video: true,
+        screen: false
+    });
+
+    // Associates the stream to the client
+    localStream.init(function() {
+
+        //Plays the localVideo
+        localStream.play('me');
+
+        //Publishes the stream to the channel
+        client.publish(localStream, handleFail);
+
+    },handleFail);
+
+},handleFail);
+//When a stream is added to a channel
+client.on('stream-added', function (evt) {
+    client.subscribe(evt.stream, handleFail);
+});
+//When you subscribe to a stream
+client.on('stream-subscribed', function (evt) {
+    let stream = evt.stream;
+    addVideoStream(stream.getId());
+    stream.play(stream.getId());
+    addCanvas(stream.getId());
+});
+//When a person is removed from the stream
+client.on('stream-removed',removeVideoStream);
+client.on('peer-leave',removeVideoStream);
